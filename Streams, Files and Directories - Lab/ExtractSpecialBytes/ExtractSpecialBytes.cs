254218ad8057c4d0f8dc311da3fdad0391ce05b4
Nikolovv23 <166643000+Nikolovv23@users.noreply.github.com>
2024-04-28 22:55:23 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,43 @@
+﻿// You are given a binary file (e. g. example.png) and a text file (e. g. bytes.txt), holding a list of bytes in the range [0…255].
+// Write a program to extract occurrences of all given bytes from the input file to an output binary file (e. g. output.bin).
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using StreamReader streamReader = new StreamReader(bytesFilePath);
+            byte[] fileBytes = File.ReadAllBytes(binaryFilePath);
+            var bytesList = new List<String>();
+            var sb = new StringBuilder();
+
+            while (!streamReader.EndOfStream)
+            {
+                bytesList.Add(streamReader.ReadLine());
+            }
+            foreach (var item in fileBytes)
+            {
+                if (bytesList.Contains(item.ToString()))
+                {
+                    sb.AppendLine(item.ToString());
+                }
+
+            }
+            using StreamWriter file = new System.IO.StreamWriter(outputPath);
+            file.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
---
+++ Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,30 @@
+﻿// You are given a folder in the file system (e. g. TestFolder). Calculate the size of all files in the folder and its subfolders.
+// The result should be written to another text (e. g. оutput.txt) file in kilobytes.
+using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            FileInfo[] infos = dir.GetFiles("*.txt", SearchOption.AllDirectories);
+            foreach (FileInfo fileInfo in infos)
+            {
+                sum += fileInfo.Length;
+            }
+            sum = sum / 1024 / 1024;
+            File.WriteAllText(outputFilePath, sum.ToString());
+        }
+    }
+}
