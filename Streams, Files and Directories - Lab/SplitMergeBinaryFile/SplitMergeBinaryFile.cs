cd4ae6831906d0bb35bbf2ff965bb27e1133e74f
Nikolovv23 <166643000+Nikolovv23@users.noreply.github.com>
2024-04-29 19:14:23 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/SplitMergeBinaryFile/SplitMergeBinaryFile.cs
@@ -0,0 +1,65 @@
+﻿// You are given an input binary file (e. g. example.png). Write a program to split it into two equal-sized files (e. g. part-1.bin and part-2.bin).
+// When the input file size is an odd number, the first part should be 1 byte bigger than the second.
+// After splitting the input file, join the obtained files into a new file(e.g.example - joined.png).
+// The obtained result file should be the same as the initial input file.
+using System.IO;
+
+namespace SplitMergeBinaryFile
+{
+    public class SplitMergeBinaryFile
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"..\..\..\Files\example.png";
+            string joinedFilePath = @"..\..\..\Files\example-joined.png";
+            string partOnePath = @"..\..\..\Files\part-1.bin";
+            string partTwoPath = @"..\..\..\Files\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using (var source = new FileStream(sourceFilePath, FileMode.Open))
+            {
+                using (var firstPart = new FileStream(partOneFilePath, FileMode.Create))
+                {
+                    int odd = source.Length % 2 == 1 ? 1 : 0;
+
+                    byte[] buffer = new byte[source.Length / 2 + odd];
+                    source.Read(buffer);
+                    firstPart.Write(buffer);
+                }
+
+                using (var secondPart = new FileStream(partTwoFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[source.Length / 2];
+                    source.Read(buffer);
+                    secondPart.Write(buffer);
+                }
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (var joined = new FileStream(joinedFilePath, FileMode.Create))
+            {
+                using (var firstPart = new FileStream(partOneFilePath, FileMode.Open))
+                {
+
+                    byte[] buffer = new byte[firstPart.Length];
+                    firstPart.Read(buffer);
+                    joined.Write(buffer);
+                }
+
+                using (var secondPart = new FileStream(partTwoFilePath, FileMode.Open))
+                {
+                    byte[] buffer = new byte[secondPart.Length];
+                    secondPart.Read(buffer);
+                    joined.Write(buffer);
+                }
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories - Lab/WordCount/WordCount.cs
@@ -0,0 +1,66 @@
+﻿// Write a program that reads a list of words from a given file (e. g. words.txt) and finds how many times each of the words occurs in another file (e. g. text.txt).
+// Matching should be case-insensitive. The result should be written to an output text file (e. g. output.txt). Sort the words by frequency in descending order.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            using (StreamReader readWords = new StreamReader(wordsFilePath))
+            {
+                using (StreamReader readText = new StreamReader(textFilePath))
+                {
+                    string[] words = readWords.ReadToEnd().Split(" ").ToArray();
+                    Dictionary<string, int> wordList = new Dictionary<string, int>();
+                    foreach (string word in words)
+                    {
+                        wordList.Add(word, 0);
+                    }
+                    while (!readText.EndOfStream)
+                    {
+                        string[] currentText = readText.ReadLine()
+                        .ToLower()
+                        .Replace("-", " ")
+                        .Replace(".", " ")
+                        .Replace("...", " ")
+                        .Replace(",", " ")
+                        .Replace("!", " ")
+                        .Replace("?", " ")
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                        foreach (string currentWord in currentText)
+                        {
+                            if (wordList.ContainsKey(currentWord))
+                            {
+                                wordList[currentWord]++;
+                            }
+                        }
+                    }
+
+                    using (StreamWriter writeWords = new StreamWriter(outputFilePath))
+                    {
+                        var orderedList = wordList.OrderByDescending(word => word.Value);
+                        foreach (var word in orderedList)
+                        {
+                            writeWords.WriteLine($"{word.Key} - {word.Value}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
